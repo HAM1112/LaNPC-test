5a51c3517d19b878f7079271e1f1abbf79d23cac
Hamdan Salih <hamdansalihpc@gmail.com>
2023-10-04 20:52:46 +0530

    add media file root

--- LaNPC/urls.py
+++ LaNPC/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('paypal/' , include('paypal.standard.ipn.urls')),
 ] 
 
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
