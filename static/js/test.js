44648e285cb33f18ee9c61e67a53492d5d1594f4
Hamdan Salih <hamdansalihpc@gmail.com>
2023-10-04 12:22:26 +0530

    static file test

---
+++ static/js/test.js
@@ -0,0 +1,5 @@
+console.log("test js ....test.js......1");
+console.log("test js ....test.js......2");
+console.log("test js ....test.js......3");
+console.log("test js ....test.js......4");
+console.log("test js ....test.js......5");
